185f9db9c61e614267a694f0ed5c67a807314860
paulpod <paulpod@neuromantics.net>
2015-02-10 15:54:25 +0000

    made a two step process, pick ref type first then enter it

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,42 @@ module.exports = {
       res.render('index');
     });
 
-    // add your routes here
+    
+
+
+
+
+
+    /* - - - - - - - - - - - - - - - - - - - */
+    /* Pages for EVL revision to entrypoint */
+
+    app.get('/examples/elements/evl-3-got', function (req, res) {
+
+    var x = req.query.reftype;
+
+    
+
+    if (x == 1) {
+    	console.log(x);
+    	res.render('examples/elements/evl-3-v11');
+    } else if (x == 2) {
+    	console.log(x);
+    	res.render('examples/elements/evl-3-v5c');
+    } else if (x == 3) {
+    	console.log(x);
+    	res.render('examples/elements/evl-3-v5c2');
+    }
+
+
+ 
+	});
+
+
+    
+
+    
+    
+
 
   }
 };
